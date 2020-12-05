a61e9be0e73fda3d6e4fd71e1a84179f0d56810f
abc <abc@example.com>
2020-12-05 10:45:56 +0700

    add function loading mycourses of user

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,25 @@
 const jwt = require("jsonwebtoken");
-const { isProtectedCSRF } = require('../libs/utils');
+const { isProtectedCSRF } = require("../libs/utils");
 
 module.exports = {
   authenticate: async (req, res, next) => {
-    if (!isProtectedCSRF(req)) {
+    // if (!isProtectedCSRF(req)) {
+    //   res.status(401).json({
+    //     message: "Authenticate failed : x-requested-with missing",
+    //   });
+    // }
+    // // Lấy full JWT từ 2 cookie
+    // else {
+    const fullJwt = req.cookies.headerAndPayload + "." + req.cookies.signature;
+    try {
+      req.decoded = await jwt.verify(fullJwt, process.env.SECRET_KEY);
+      next();
+    } catch (error) {
       res.status(401).json({
-        message: "Authenticate failed : x-requested-with missing",
+        message: "Authenticated failed",
       });
     }
-    // Lấy full JWT từ 2 cookie
-    else {
-      const fullJwt =
-        req.cookies.headerAndPayload + "." + req.cookies.signature;
-      try {
-        req.decoded = await jwt.verify(fullJwt, process.env.SECRET_KEY);
-        next();
-      } catch (error) {
-        res.status(401).json({
-          message: "Authenticated failed",
-        });
-      }
-    }
+
     // }
   },
   verify: (req, res, next) => {
--- routes/profile.js
+++ routes/profile.js
@@ -1,17 +1,75 @@
-const express = require('express');
+const express = require("express");
+const { authenticate } = require("../middlewares/auth");
+const User = require("../models/User");
 const router = express.Router();
 
+router.get("/teacher", (req, res) => res.render("profile/teacher"));
 
+router.get("/student", authenticate, async (req, res) => {
+  try {
+    const idUser = req.decoded._id;
+    const existedUser = await User.findById(idUser).lean();
 
-router.get("/teacher",(req,res) => res.render("profile/teacher"));
-router.get("/student",(req,res) => res.render("profile/student"));
-router.get("/student/mycourse",(req,res) => res.render("profile/student/mycourse"));
-router.get("/student/wishlist",(req,res) => res.render("profile/student/wishlist"));
-router.get("/student/edit",(req,res) => res.render("profile/student/edit"));
+    res.render("profile/student", { user: existedUser });
+  } catch (err) {
+    res.status(400).json({
+      message: err,
+    });
+  }
+});
 
+router.get("/student/edit", authenticate, async (req, res) => {
+  try {
+    const idUser = req.decoded._id;
+    const existedUser = await User.findById(idUser).lean();
+    res.render("profile/student/edit", { user: existedUser });
+  } catch (err) {
+    res.status(400).json({
+      message: err,
+    });
+  }
+});
 
+router.post("/student/edit", authenticate, async (req, res) => {
+  try {
+    const idUser = req.decoded._id;
+    const { userName, age, address, degree, phone } = req.body;
+    const existedUser = await User.findById(idUser);
+    existedUser.userName = userName;
+    existedUser.degree = degree;
+    existedUser.age = age;
+    existedUser.phone = phone;
+    existedUser.address = address;
+    existedUser.save();
+    res.render("profile/student/edit", { user: existedUser });
+  } catch (err) {
+    res.status(400).json({
+      message: err,
+    });
+  }
+});
 
+router.get("/student/mycourse", authenticate, async (req, res) => {
+  try {
+    const idUser = req.decoded._id;
+    const existedUser = await User.findById(idUser)
+      .populate({
+        path: "myCourses",
+        populate: {
+          path: "teacher",
+        },
+      })
+      .lean();
+    res.render("profile/student/mycourse", { user: existedUser });
+  } catch (err) {
+    res.status(400).json({
+      message: err,
+    });
+  }
+});
 
-
+router.get("/student/wishlist", (req, res) =>
+  res.render("profile/student/wishlist")
+);
 
 module.exports = router;
